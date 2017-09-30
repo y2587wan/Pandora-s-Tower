66dc957196ac111a2f220fb85509a1165fb8ae9b
yuanxinwang <alexyuanxinwang@gmail.com>
2017-09-30 18:14:40 -0400

    Make some Prefab

--- Assets/Script/ButtonControl.cs
+++ Assets/Script/ButtonControl.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonControl : MonoBehaviour {
+public class ButtonControl : MonoBehaviour
+{
 
     private bool isPressByPlayer = false;
     public Text GameWinText;
+    public GameObject ClearObject;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Player")
         {
             isPressByPlayer = true;
-            Time.timeScale = 0;
+            //Time.timeScale = 0;
             Debug.Log("You win!");
             GameWinText.gameObject.SetActive(true);
+            ClearObject.SetActive(true);
         }
     }
 }
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -3,47 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
 
 
     public float speed;
-    public float vertialSpeed;
+    public float addForce = 800;
+    public float jumpForce;
     public Text scoreText;
     public KeyCode jump;
     public KeyCode moveLeft;
     public KeyCode moveRight;
+    public float startForce = 100;
 
+    private Vector2 force;
     private int score = 0;
     private Rigidbody2D rb2d;
-    private bool isGround = true;
-    private float moveVertical;
+    private bool onGround = true;
     private float moveHorizontal;
+    private bool canJump = true;
+
+    float JumpForce
+    {
+        get { return jumpForce; }
+        set
+        {
+            if (value <= 0)
+                jumpForce = 0;
+            else
+                jumpForce = value;
+        }
+    }
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        jumpForce = startForce;
         SetCountText();
     }
 
     void Update()
     {
+        Jump();
         MovementControl();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Jump()
     {
-        if (collision.transform.tag == "Ground")
+        if (Input.GetKey(jump) && canJump)
         {
-            isGround = true;
+            JumpForce -= Time.deltaTime * addForce;
+            PlayerJump();
         }
-        else
+
+        if (Input.GetKeyUp(jump) && !onGround)
         {
-            isGround = false;
+            canJump = false;
         }
+    }
+
+    void PlayerJump()
+    {
+        force = new Vector2(0, jumpForce);
+        rb2d.AddForce(force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Ground")
+        {
+            JumpForce = startForce;
+            canJump = true;
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Ground")
+            onGround = false;
     }
 
     private void MovementControl()
     {
-        
+
 
         if (Input.GetKey(moveLeft))
             moveHorizontal = -1;
@@ -52,18 +95,8 @@ public class PlayerControl : MonoBehaviour {
         else
             moveHorizontal = 0;
 
-        if (isGround && Input.GetKeyDown(jump))
-        {
-            moveVertical = vertialSpeed;
-            isGround = false;
-        }
-        else
-        {
-            moveVertical = 0;
-        }
-        
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        Vector2 movement = new Vector2(moveHorizontal * speed, 0);
+        rb2d.AddForce(movement);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
