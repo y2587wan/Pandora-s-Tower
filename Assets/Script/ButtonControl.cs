34a2c4d924f1ad79e729ce9d8ef550f96a3dfe1e
Alex Wang <y2587wan@edu.uwaterloo.ca>
2017-09-29 20:53:40 -0400

    Set Up Platform
    Set up basic things

---
+++ Assets/Script/ButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour {
+
+    private bool isPressByPlayer = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            isPressByPlayer = true;
+            Debug.Log("You win!");
+        }
+    }
+}
---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+    public float speed;
+    public float vertialSpeed;
+
+    private Rigidbody2D rb2d;
+    private bool isGround = true;
+    private float moveVertical;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MovementControl();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.transform.name);
+        if (collision.transform.tag == "Ground")
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+    }
+
+    private void MovementControl()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (isGround && Input.GetKeyDown("space"))
+        {
+            moveVertical = vertialSpeed;
+            Debug.Log("Jump");
+            isGround = false;
+        }
+        else
+        {
+            moveVertical = 0;
+        }
+        
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+
+}
